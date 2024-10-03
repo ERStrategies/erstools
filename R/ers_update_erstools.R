99e89f2c189b104963c2933a35ccf0714c8efbcb
Zach Friedman <zfriedman@erstrategies.org>
2024-10-03 15:17:46 -0400

    ers_update_erstools and markdown template complete for JK review!

---
+++ R/ers_update_erstools.R
@@ -0,0 +1,31 @@
+#' Update the erstools package
+#'
+#'
+#' @param pkg_name OPTIONAL: The private github package you want to install. Currently only supports erstools.
+#' @param upgrade OPTIONAL: By default it installs all other necessary packages. It will give warnings if some of them failed.
+#' @return Nothing. Your package will be installed
+#' @export
+#' @import devtools
+ers_update_erstools <- function(pkg_name = "erstools", upgrade = "always") {
+  # Check if the package is already loaded
+  if (pkg_name %in% loadedNamespaces()) {
+    message("Package is loaded. Detaching package before updating...")
+
+    # Detach the package
+    tryCatch({
+      detach(paste0("package:", pkg_name), unload = TRUE, character.only = TRUE)
+      message("Package detached.")
+    }, error = function(e) {
+      message("Error while detaching: ", e$message)
+    })
+  }
+
+  # Attempt to install/update the package from GitHub
+  tryCatch({
+    devtools::install_github("ERStrategies/erstools", auth_token = Sys.getenv("GITHUB_PAT"), force = TRUE, upgrade = upgrade)
+    message("Package successfully installed/updated.")
+  }, error = function(e) {
+    message("Error during installation: ", e$message)
+  })
+  library(erstools)
+}
