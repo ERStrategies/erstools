a421f3ff69046043dc877326360648979a2be973
Zach Friedman <zfriedman@erstrategies.org>
2024-12-05 09:42:05 -0500

    updates to ers_update_erstools

--- R/ers_update_erstools.R
+++ R/ers_update_erstools.R
@@ -5,35 +5,44 @@
 #' @param upgrade OPTIONAL: By default it installs all other necessary packages. It will give warnings if some of them failed.
 #' @return Nothing. Your package will be installed
 #' @export
-#' @import devtools
-#' @import utils
+#' @importFrom devtools install_github
+#' @importFrom utils installed.packages remove.packages
 ers_update_erstools <- function(pkg_name = "erstools", upgrade = "always") {
-  # Check if the package is already loaded
+  # Ensure namespace is unloaded
   if (pkg_name %in% loadedNamespaces()) {
-    message("Package is loaded. Detaching package before updating...")
-
-    # Detach the package
+    message("Package is loaded. Unloading package before updating...")
     tryCatch({
-      detach(paste0("package:", pkg_name), unload = TRUE, character.only = TRUE)
-      message("Package detached.")
+      unloadNamespace(pkg_name)
+      message("Namespace unloaded.")
     }, error = function(e) {
-      message("Error while detaching: ", e$message)
+      message("Error while unloading namespace: ", e$message)
     })
   }
 
-  # Remove the package completely if it is installed
+  # Remove the package completely
   if (pkg_name %in% installed.packages()) {
     tryCatch({
-      remove.packages(pkg_name)
+      remove.packages(pkg_name, lib = .libPaths()[1])
       message("Package successfully removed.")
     }, error = function(e) {
       message("Error during removal: ", e$message)
     })
   }
 
-  # Attempt to install/update the package from GitHub
+  # Clear cached help database
   tryCatch({
-    devtools::install_github("ERStrategies/erstools", auth_token = Sys.getenv("GITHUB_PAT"), force = TRUE, upgrade = upgrade)
+    unlink(file.path(.libPaths()[1], pkg_name), recursive = TRUE, force = TRUE)
+    message("Cached files cleared.")
+  }, error = function(e) {
+    message("Error clearing cached files: ", e$message)
+  })
+
+  # Install/update the package from GitHub
+  tryCatch({
+    devtools::install_github("ERStrategies/erstools",
+                             auth_token = Sys.getenv("GITHUB_PAT"),
+                             force = TRUE,
+                             upgrade = upgrade)
     message("Package successfully installed/updated.")
   }, error = function(e) {
     message("Error during installation: ", e$message)
